b5f87981c4839cf7f3eb31a3dc467c95f391d4d9
waikin1216 <waikin1216@gmail.com>
2024-01-12 14:59:01 +0800

    Initial Commit

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by Wai Kin Ng on 12/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
